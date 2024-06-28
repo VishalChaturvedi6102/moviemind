17e0f8b4cd5eecaba597c86fc974ae3ae4f9c3d0
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2024-06-28 23:49:10 +0530

    created hooks folder and completed my maincontainer and videotitle file ; again encountaring some errors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,16 +2,15 @@ export const LOGO =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
 export const USER_AVATAR =
-  "https://avatars.githubusercontent.com/u/91264004?v=4";
+  "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
 export const API_OPTIONS = {
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlM2UwYTg4MWUwZWQ4MWY0OWU3OWM4YzZkNGFhNWQ2NSIsIm5iZiI6MTcxOTUxMDk2MS41Mzc0MjgsInN1YiI6IjY2N2RhMzgzYmE3N2JkM2I0NTI1MTczZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NMpQp0UJ5x7Phd1_yGHD5Z2n0UGM1kL0I0-mw4zo0Q4'
-    }
-  };
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+  },
+};
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
